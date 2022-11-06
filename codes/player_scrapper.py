24d343affb65517571c552fef063d6c1eeb18bdf
Avisionary <aa2464@georgetown.edu>
2022-11-06 14:56:01 -0500

    add docstrings to player scrapper

--- codes/player_scrapper.py
+++ codes/player_scrapper.py
@@ -6,6 +6,45 @@ import regex as re
 from team_scrapper import TeamScrapper
 
 class PlayerScrapper:
+    """
+    A class to scrape La Liga players data and stores into a dataframe
+    
+    ----------
+    Attributes
+    ----------
+    season:str
+        Gets the season for which data needs to be collected
+    players: list
+        A list to append player names
+    teamURL: list
+        A list to append team URLs
+    age: list
+        A list to append age of each player
+    dob: list
+        A list to append date of birth of players
+    shirt_number: list
+        A list to append shirt number of the players
+    foot: list
+        A list to append strong foot of the players
+    mv: list
+        A list to append market value of the players
+    joined: list
+        A list to append date of joining of players
+    contract: list
+        A list to append date of contract of players
+
+    -------
+    Methods
+    -------
+    set_url: Changes URL to go the detailed version of every player's table
+    __get_players: Uses the HTML tag td and class posrela to get all player names and their positions
+    __get_player_stats: Uses the HTML tag td and class zentriert to get all player stats
+    __get_mv: Uses the HTML tag td and class rechts hauptlink to get all player market value
+    __to_df : Creates a dictonary of all the appended lists and turns it into a pandas DF
+    __to_csv : Saves the pandas DF to a csv file in the specified location
+    use_player_scrapper : Calls the methods in a desired order to use the scrapper
+    
+    """
 
     def __init__(self,season):
         self.season = season
@@ -33,6 +72,7 @@ class PlayerScrapper:
 
 
     def set_url(self,teamURL):
+        '''Takes in the URL, returns the string into a desired format'''
         temp = teamURL
         temp = f"https://www.transfermarkt.us{temp}/plus/1"
         #print(temp)
@@ -41,6 +81,7 @@ class PlayerScrapper:
 
 
     def __get_players(self,pageSoup,players,position):
+        '''Takes in the HTML of the URL, appends player name and position to the specific list'''
         counter = 0
         for test in pageSoup.findAll('td', class_ = "posrela"):
             for row in test.findAll("tr"):
@@ -59,6 +100,7 @@ class PlayerScrapper:
 
 
     def __get_player_stats(self,pageSoup,shirt_number,dob,age,height,foot,joined,contract):
+        '''Takes in the HTML of the URL, appends player stats to desired lists'''
         counter = 0
         for x in pageSoup.findAll('table',class_ = "items"):
             for test in x.findAll('td', class_ = "zentriert"):
@@ -97,6 +139,7 @@ class PlayerScrapper:
 
     
     def __get_mv(self,pageSoup,mv,teams,team):
+        '''Takes in the HTML of the URL, appends market value of the player to desired list'''
         for test in pageSoup.findAll('td',class_ = "rechts hauptlink"):
             if test != "":
                 d = test.text
@@ -106,6 +149,7 @@ class PlayerScrapper:
 
 
     def __to_df(self):
+        '''Takes in the HTML of the URl, appends team name and URL to the specific list'''
         # dictionary of lists 
         dict = {"player" : self.players,
                     'position' : self.position,
@@ -124,12 +168,14 @@ class PlayerScrapper:
 
 
     def __to_csv(self,location):
+        '''Takes in a location, and saves the dataframe to that location'''
         self.players_df.to_csv(location,index = False)
 
 
 
 
     def use_player_scrapper(self):
+        '''Calls all the methods in the desired order, call this function on the class instantiation'''
         for i in range(len(self.teamURL)):
             team = self.team[i]
             url = self.teamURL[i]
